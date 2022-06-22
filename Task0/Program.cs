a9548c229e73c45fbf4143fed37c62e78e451775
Dmitriy <Fes08-09@yandex.ru>
2022-06-22 18:47:54 +0300

    Task0 done

---
+++ Task0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using static System.Console; // если будет видеть неизвестную команду, он будет ее искать в класс Console и везде можем убрать надпись Console
+
+Clear();
+WriteLine("Введите число"); 
+int number = Convert.ToInt32(ReadLine()); // чтобы конвертировать числа в строки можем исп: 
+int number1 = int.Parse(ReadLine());
+
+int sqrNumber = number * number;
+int sqrNumber1 = Convert.ToInt32(Math.Pow(number1, 2)); // возводим в квадрат двумя способами
+
+WriteLine(sqrNumber);
